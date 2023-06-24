b20f357a1d4385c3a569abb708ccda069427d8eb
SUJAL676 <100483723+SUJAL676@users.noreply.github.com>
2023-06-24 21:26:21 +0530

    comment card report

--- lib/widget/comment_card.dart
+++ lib/widget/comment_card.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Comment_Card extends StatefulWidget {
   final String postid;
@@ -9,10 +11,18 @@ class Comment_Card extends StatefulWidget {
   State<Comment_Card> createState() => _Comment_CardState();
 }
 
-class _Comment_CardState extends State<Comment_Card> {
+class _Comment_CardState extends State<Comment_Card>
+{
+
+
   @override
-  Widget build(BuildContext context) {
-    return Container(
+  Widget build(BuildContext context)
+  {
+   DateTime dateTime=widget.snapshot['date_time'].toDate();
+   String formatted_date=DateFormat('dd/MM/yy').format(dateTime);
+
+    return Container
+      (
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
@@ -30,7 +40,7 @@ class _Comment_CardState extends State<Comment_Card> {
                   Text(widget.snapshot['desc']),
                 ],),
               SizedBox(height: 3,),
-              Text("11/04/23"),
+              Text(formatted_date),
               SizedBox(height: 25,)
             ],
           )
