cb590afad460b166d9aa1d514625fe279f13e5fd
SUJAL676 <100483723+SUJAL676@users.noreply.github.com>
2023-06-29 19:59:15 +0530

    Signup setup

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -17,79 +17,155 @@ class _Search_ScreenState extends State<Search_Screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: mobileBackgroundColor,
-        centerTitle: false,
-        title: Expanded(
-          child: Container(
-            height: 50,
-            width: double.infinity,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10) ,color: Colors.white12),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(padding: EdgeInsets.only(top: 11.6,left: 10,right: 10),
-                    child: Icon(Icons.search)),
-                Expanded(
-                  child: TextField(
-                    controller: controller,
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Enter username"
-                    ),
-                    onSubmitted: (String _)
-                    {
-                      setState(() {
-                        _isbool=true;
-                      });
-                    }
-                    ,
-                  ),
-                )
-              ],
-            ),
-          ),
-        )
-      ),
+      // appBar: AppBar(
+      //   automaticallyImplyLeading: false,
+      //   backgroundColor: mobileBackgroundColor,
+      //   centerTitle: false,
+      //   title: Expanded(
+      //     child: Container(
+      //       height: 50,
+      //       width: double.infinity,
+      //       decoration: BoxDecoration(borderRadius: BorderRadius.circular(10) ,color: Colors.white12),
+      //       child: Row(
+      //         crossAxisAlignment: CrossAxisAlignment.start,
+      //         mainAxisAlignment: MainAxisAlignment.start,
+      //         children: [
+      //           Padding(padding: EdgeInsets.only(top: 11.6,left: 10,right: 10),
+      //               child: Icon(Icons.search)),
+      //           Expanded(
+      //             child: TextField(
+      //               controller: controller,
+      //               decoration: InputDecoration(
+      //                   border: InputBorder.none,
+      //                   hintText: "Enter username"
+      //               ),
+      //               onSubmitted: (String _)
+      //               {
+      //                 setState(() {
+      //                   _isbool=true;
+      //                 });
+      //               }
+      //               ,
+      //             ),
+      //           )
+      //         ],
+      //       ),
+      //     ),
+      //   )
+      // ),
 
-      body:_isbool?FutureBuilder(
-        future: FirebaseFirestore.instance.collection('users').where('username',isEqualTo: controller.text).get(),
-        builder: (context,snapshot)
-        {
-          if(snapshot.connectionState==ConnectionState.waiting)
-            {
-              CircularProgressIndicator();
-            }
-          else
-            {
-              return ListView.builder(
-                  itemCount: (snapshot.data! as dynamic).docs.length,
-                  itemBuilder: (context , index)
-                  {
-                    return InkWell(
-                      onTap: (){
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context)=> Profile_Screen(photourl: (snapshot.data! as dynamic).docs[index]['image'],
-                              desc: (snapshot.data! as dynamic).docs[index]['bio'],
-                              username: (snapshot.data! as dynamic).docs[index]['username'],
-                              uid: (snapshot.data! as dynamic).docs[index]['uid'], isback: true,
-                              // followers: (snapshot.data! as dynamic).docs[index]['followers'],
-                              // following: (snapshot.data! as dynamic).docs[index]['following'],
-                            )) );
-                      },
-                      child: ListTile(
-                        leading: CircleAvatar(backgroundImage: NetworkImage((snapshot.data! as dynamic).docs[index]['image']),),
-                        title: Text((snapshot.data! as dynamic).docs[index]['username']),
+      // body:_isbool?FutureBuilder(
+      //   future: FirebaseFirestore.instance.collection('users').where('username',isEqualTo: controller.text).get(),
+      //   builder: (context,snapshot)
+      //   {
+      //     if(snapshot.connectionState==ConnectionState.waiting)
+      //       {
+      //         CircularProgressIndicator();
+      //       }
+      //     else
+      //       {
+      //         return ListView.builder(
+      //             itemCount: (snapshot.data! as dynamic).docs.length,
+      //             itemBuilder: (context , index)
+      //             {
+      //               return InkWell(
+      //                 onTap: (){
+      //                   Navigator.of(context).push(MaterialPageRoute(
+      //                       builder: (context)=> Profile_Screen(photourl: (snapshot.data! as dynamic).docs[index]['image'],
+      //                         desc: (snapshot.data! as dynamic).docs[index]['bio'],
+      //                         username: (snapshot.data! as dynamic).docs[index]['username'],
+      //                         uid: (snapshot.data! as dynamic).docs[index]['uid'], isback: true,
+      //                         // followers: (snapshot.data! as dynamic).docs[index]['followers'],
+      //                         // following: (snapshot.data! as dynamic).docs[index]['following'],
+      //                       )) );
+      //                 },
+      //                 child: ListTile(
+      //                   leading: CircleAvatar(backgroundImage: NetworkImage((snapshot.data! as dynamic).docs[index]['image']),),
+      //                   title: Text((snapshot.data! as dynamic).docs[index]['username']),
+      //                 ),
+      //               );
+      //             }
+      //         );
+      //       }
+      //     return Center();
+      //   } ,
+      // ) : post_display()
+
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 15).copyWith(top: 10),
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10) ,color: Colors.white12),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(padding: EdgeInsets.only(top: 11.6,left: 10,right: 10),
+                      child: Icon(Icons.search)),
+                  Expanded(
+                    child: TextField(
+                      controller: controller,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Enter username"
                       ),
-                    );
-                  }
-              );
-            }
-          return Center();
-        } ,
-      ) : post_display()
+                      onSubmitted: (String _)
+                      {
+                        setState(() {
+                          _isbool=true;
+                        });
+                      }
+                      ,
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            // Container(
+            //   child: _isbool?FutureBuilder(
+            //       future: FirebaseFirestore.instance.collection('users').where('username',isEqualTo: controller.text).get(),
+            //       builder: (context,snapshot)
+            //       {
+            //         if(snapshot.connectionState==ConnectionState.waiting)
+            //           {
+            //             CircularProgressIndicator();
+            //           }
+            //         else
+            //           {
+            //             return ListView.builder(
+            //                 itemCount: (snapshot.data! as dynamic).docs.length,
+            //                 itemBuilder: (context , index)
+            //                 {
+            //                   return InkWell(
+            //                     onTap: (){
+            //                       Navigator.of(context).push(MaterialPageRoute(
+            //                           builder: (context)=> Profile_Screen(photourl: (snapshot.data! as dynamic).docs[index]['image'],
+            //                             desc: (snapshot.data! as dynamic).docs[index]['bio'],
+            //                             username: (snapshot.data! as dynamic).docs[index]['username'],
+            //                             uid: (snapshot.data! as dynamic).docs[index]['uid'], isback: true,
+            //                             // followers: (snapshot.data! as dynamic).docs[index]['followers'],
+            //                             // following: (snapshot.data! as dynamic).docs[index]['following'],
+            //                           )) );
+            //                     },
+            //                     child: ListTile(
+            //                       leading: CircleAvatar(backgroundImage: NetworkImage((snapshot.data! as dynamic).docs[index]['image']),),
+            //                       title: Text((snapshot.data! as dynamic).docs[index]['username']),
+            //                     ),
+            //                   );
+            //                 }
+            //             );
+            //           }
+            //         return Center();
+            //       } ,
+            //     ) : post_display(),
+            // )
+          ],
+        ),
+      ),
 
 
 
